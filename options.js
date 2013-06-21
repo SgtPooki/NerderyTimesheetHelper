cf026fda635a7fb96bbbd1b9197ed9711cc3c015
Paul Schoenfelder <paulschoenfelder@gmail.com>
2013-06-21 04:33:11 -0500

    Clean up options code

--- options.js
+++ options.js
@@ -2,6 +2,8 @@
 
     var options = {
 
+        keybinder: new Keybindings(),
+
         elements: {
             $status:   $('#status'),
             $form:     $('form'),
@@ -14,29 +16,29 @@
             this.elements.$status.html(message).fadeIn('fast').delay(1000).fadeOut('fast');
         },
         error: function(message) {
-            this.elements.$status.removeClass('success error').addClass('error')
+            this.elements.$status.removeClass('success error').addClass('error');
             this.elements.$status.html(message).fadeIn('fast').delay(1000).fadeOut('fast');
             // Show debugging info if provided
             if (arguments.length > 1) {
-                console.debug(utils.slice(arguments, 1));
+                toolbox.log.debug(toolbox.slice(arguments, 1));
             }
         },
 
         // Persistence
         // Saves options to localStorage.
         save: function() {
-            localStorage['Keybindings'] = Keybindings.serialize();
+            localStorage['Keybindings'] = this.keybinder.serialize();
             this.success('Options Saved.');
         },
         // Restores options from localStorage
         restore: function() {
             var restored = localStorage['Keybindings'];
             if (restored) {
-                Keybindings.deserialize(restored);
-            } 
+                this.keybinder.deserialize(restored);
+            }
 
             // Display keybindings
-            Keybindings.bindings.forEach(function(binding) {
+            this.keybinder.bindings.forEach(function(binding) {
                 var $input = $('#' + binding.name);
                 $input.val(binding.combo.toString());
             });
@@ -58,40 +60,43 @@
             });
 
             // Wire up key bind input fields
-            this.elements.$keybinds.on('keydown', self.bindKeys);
+            this.elements.$keybinds.on('keydown', function(e) {
+                self.bindKeys(e, $(this));
+            });
         },
 
         loadDefaults: function() {
+            var self = this;
             // Load default keybindings from name and data-default-bind attributes on data-keybinder inputs
             this.elements.$keybinds.each(function(_, input) {
                 var $input   = $(input);
                 var name     = $input.attr('name');
                 var comboStr = $input.attr('data-default-bind');
                 if (name && comboStr) {
-                    Keybindings.add(name, Combo.fromString(comboStr));
+                    self.keybinder.add(name, Combo.fromString(comboStr));
                 }
             });
         },
 
-        bindKeys: function(e) {
+        bindKeys: function(e, $element) {
             // Get the current key combo
             var combo = Combo.fromEvent(e);
 
             // On ESC or BACKSPACE, clear the input
             if (Keys.isKeyPressed([ Keys.Esc, Keys.Backspace ], e.which) && !combo.containsMetaKeys()) {
-                $(this).val('');
-            } 
+                $element.val('');
+            }
             // Allow normal tab behavior
             else if (Keys.isKeyPressed(Keys.Tab, e.which) && !combo.containsMetaKeys(combo)) {
                 return;
-            } 
+            }
             // Otherwise, bind the current key combination
             else {
-                var name = $(this).attr('name');
+                var name = $element.attr('name');
                 // Create new binding or update the current keybinding
-                Keybindings.add(name, combo);
+                this.keybinder.add(name, combo);
                 // Display new binding
-                $(this).val(combo.toString());
+                $element.val(combo.toString());
             }
             return false;
         }
